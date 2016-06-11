9a581933222d62bfcc0463dde6b643c7baf676e3
Emmy <emmy.ma527@gmail.com>
2016-06-11 09:21:27 +0000

    20160611

---
+++ features/step_definitions/basic_step.rb
@@ -0,0 +1,7 @@
+Given(/^I am on homepage$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)"$/) do |text|
+  pending # Write code here that turns the phrase above into concrete actions
+end
